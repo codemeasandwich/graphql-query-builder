2b180c6f1b229c61dedeb2e4bdff52c25a62f2a6
Marc Beaupre <marc.beaupre@autodesk.com>
2019-03-11 14:27:30 +0800

    m: Typo Change: passing `undefined` not an Error

--- test.js
+++ test.js
@@ -12,67 +12,67 @@ function removeSpaces(textS) {
 
 describe('graphql query builder', function() { // log the function
 	it('should accept a single find value', function() {
-		const expeted = `query{user{age}}`;
+		const expected = `query{user{age}}`;
 		const user = new Query('user').select('age');
 
-		expect(removeSpaces(expeted)).to.equal(removeSpaces(user));
+		expect(removeSpaces(expected)).to.equal(removeSpaces(user));
 	});
 
 	it('should create a Query with function name & alias', function() {
 
-		const expeted = `query{sam : user{name}}`;
+		const expected = `query{sam : user{name}}`;
 		const user = new Query('user', 'sam').select('name');
 
-		expect(removeSpaces(expeted)).to.equal(removeSpaces(user));
+		expect(removeSpaces(expected)).to.equal(removeSpaces(user));
 	});
 
 	it('should create a Query with function name & input', function() {
 
-		const expeted = `query{user(id:12345){name}}`;
+		const expected = `query{user(id:12345){name}}`;
 		const user = new Query('user', {
 			id: 12345
 		}).select('name');
 
-		expect(removeSpaces(expeted)).to.equal(removeSpaces(user));
+		expect(removeSpaces(expected)).to.equal(removeSpaces(user));
 	});
 
 	it('should create a Query with function name & input(s)', function() {
 
-		const expeted = `query{user(id:12345, age:34){name}}`;
+		const expected = `query{user(id:12345, age:34){name}}`;
 		const user = new Query('user', {
 			id: 12345,
 			age: 34
 		}).select('name');
 
-		expect(removeSpaces(expeted)).to.equal(removeSpaces(user));
+		expect(removeSpaces(expected)).to.equal(removeSpaces(user));
 	});
 
 	it('should accept a single find value with alia', function() {
-		const expeted = `query{user{nickname:name}}`;
+		const expected = `query{user{nickname:name}}`;
 		const user = new Query('user').select({
 			nickname: 'name'
 		});
 
-		expect(removeSpaces(expeted)).to.equal(removeSpaces(user));
+		expect(removeSpaces(expected)).to.equal(removeSpaces(user));
 	});
 
 	it('should accept a multiple find values', function() {
-		const expeted = `query{user{firstname, lastname}}`;
+		const expected = `query{user{firstname, lastname}}`;
 		const user = new Query('user').select('firstname', 'lastname');
 
-		expect(removeSpaces(expeted)).to.equal(removeSpaces(user));
+		expect(removeSpaces(expected)).to.equal(removeSpaces(user));
 	});
 
 	it('should accept an array find values', function() {
-		const expeted = `query{user{firstname, lastname}}`;
+		const expected = `query{user{firstname, lastname}}`;
 		const user = new Query('user').select(['firstname', 'lastname']);
 
-		expect(removeSpaces(expeted)).to.equal(removeSpaces(user));
+		expect(removeSpaces(expected)).to.equal(removeSpaces(user));
 	});
 
 	it('should work with nesting Querys', function() {
 
-		const expeted = `query{user( id:12345 ) {
+		const expected = `query{user( id:12345 ) {
             id,  nickname : name,  isViewerFriend,
             image : profilePicture( size:50 ) {
               uri,  width,    height  }    }}`;
@@ -91,24 +91,24 @@ describe('graphql query builder', function() { // log the function
 			'image': profilePicture
 		}]);
 
-		expect(removeSpaces(expeted)).to.equal(removeSpaces(user));
+		expect(removeSpaces(expected)).to.equal(removeSpaces(user));
 	});
 
 	it('should work with simple nesting Querys', function() {
 
-		const expeted = `query{user { profilePicture { uri, width, height } }}`;
+		const expected = `query{user { profilePicture { uri, width, height } }}`;
 
 		const user = new Query('user');
 		user.select({
 			'profilePicture': ['uri', 'width', 'height']
 		});
 
-		expect(removeSpaces(expeted)).to.equal(removeSpaces(user));
+		expect(removeSpaces(expected)).to.equal(removeSpaces(user));
 	});
 
 	it('should be able to group Querys', function() {
 
-		const expeted = `query{FetchLeeAndSam { lee: user(id: "1") { name  },
+		const expected = `query{FetchLeeAndSam { lee: user(id: "1") { name  },
                                     sam: user(id: "2") { name  }  }}`;
 
 		const FetchLeeAndSam = new Query('FetchLeeAndSam');
@@ -127,12 +127,12 @@ describe('graphql query builder', function() { // log the function
 
 		FetchLeeAndSam.select(lee, sam);
 
-		expect(removeSpaces(expeted)).to.equal(removeSpaces(FetchLeeAndSam));
+		expect(removeSpaces(expected)).to.equal(removeSpaces(FetchLeeAndSam));
 	});
 
 	it('should work with nasted objects and lists', function() {
 
-		const expeted = `query{myPost:Message(type:"chat",message:"yoyo",
+		const expected = `query{myPost:Message(type:"chat",message:"yoyo",
                                 user:{name:"bob",screen:{height:1080,width:1920}},
                                 friends:[{id:1,name:"ann"},{id:2,name:"tom"}])  {
                         messageId : id, postedTime : createTime }}`;
@@ -164,13 +164,13 @@ describe('graphql query builder', function() { // log the function
 			postedTime: 'createTime'
 		});
 
-		expect(removeSpaces(expeted)).to.equal(removeSpaces(MessageQuery));
+		expect(removeSpaces(expected)).to.equal(removeSpaces(MessageQuery));
 
 	});
 
 	it('should work with objects that have help functions(will skip function name)', function() {
 
-		const expeted = 'query{inventory(toy:"jack in the box")  { id }}';
+		const expected = 'query{inventory(toy:"jack in the box")  { id }}';
 
 		const ChildsToy = {
 			toy: 'jack in the box',
@@ -182,13 +182,13 @@ describe('graphql query builder', function() { // log the function
 		const ItemQuery = new Query('inventory', ChildsToy);
 		ItemQuery.select('id');
 
-		expect(removeSpaces(expeted)).to.equal(removeSpaces(ItemQuery));
+		expect(removeSpaces(expected)).to.equal(removeSpaces(ItemQuery));
 
 	});
 
 	it('should work with nasted objects that have help functions(will skip function name)', function() {
 
-		const expeted = 'query{inventory(toy:"jack in the box")  { id }}';
+		const expected = 'query{inventory(toy:"jack in the box")  { id }}';
 
 		const ChildsToy = {
 			toy: 'jack in the box',
@@ -202,13 +202,13 @@ describe('graphql query builder', function() { // log the function
 		const ItemQuery = new Query('inventory', ChildsToy);
 		ItemQuery.select('id');
 
-		expect(removeSpaces(expeted)).to.equal(removeSpaces(ItemQuery));
+		expect(removeSpaces(expected)).to.equal(removeSpaces(ItemQuery));
 
 	});
 
 	it('should skip empty objects in filter/args', function() {
 
-		const expeted = 'query{inventory(toy:"jack in the box")  { id }}';
+		const expected = 'query{inventory(toy:"jack in the box")  { id }}';
 
 		const ChildsToy = {
 			toy: 'jack in the box',
@@ -218,7 +218,7 @@ describe('graphql query builder', function() { // log the function
 		const ItemQuery = new Query('inventory', ChildsToy);
 		ItemQuery.select('id');
 
-		expect(removeSpaces(expeted)).to.equal(removeSpaces(ItemQuery));
+		expect(removeSpaces(expected)).to.equal(removeSpaces(ItemQuery));
 	});
 
 	it('should throw Error if find input items have zero props', function() {
@@ -242,10 +242,6 @@ describe('graphql query builder', function() { // log the function
 
 	it('should throw Error if find is not valid', function() {
 		expect(() => new Query('x').select(123)).to.throw(Error);
-	});
-
-	it('should throw Error if you accidentally pass an undefined', function() {
-		expect(() => new Query('x', undefined)).to.throw(Error);
 	});
 
 	it('should throw Error it is not an input object for alias', function() {
