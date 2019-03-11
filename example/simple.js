aba997b8951a92bef1c49ebfa40a80896cc5f429
Marc Beaupre <marc.beaupre@autodesk.com>
2019-03-11 14:22:52 +0800

    Change: Upgrade simple example v2

--- example/simple.js
+++ example/simple.js
@@ -1,50 +1,70 @@
-var Query = require('../');
-
+const {
+  Query,
+  Nested
+} = require('../');
 
 /*
-{
- user( id:3500401 ) {
-    id,
-    nickname : name,
-    isViewerFriend,
-    
-    profilePicture( size:50 ) {
+  EXAMPLE 1
+  query {
+    user( id: 3500401 ) {
+      id,
+      nickname: name,
+      isViewerFriend,
+      profilePicture( size: 50 ) {
         uri,
         width,
         height
+      }
     }
   }
-}
 */
 
-let profilePicture = new Query("profilePicture",{size : 50});
-    profilePicture.find( "uri", "width", "height");
-let user = new Query("user",{id : 3500401});
-    user.find(["id", {"nickname":"name"}, "isViewerFriend",  {"image":profilePicture}]);
+const profilePicture = new Query('profilePicture', {
+  size: 50
+});
+profilePicture.select('uri', 'width', 'height');
+const user = new Query('user', {
+  id: 3500401
+});
+user.select(['id', {
+  nickname: 'name'
+}, 'isViewerFriend', {
+  image: profilePicture
+}]);
 
-  logger.log("user",user+"");
+console.log('user', user + '');
+
 /*
-
-query FetchLeeAndSam {
-  lee: user(id: "1") {
-    name
+  EXAMPLE 2
+  query {
+    FetchLeeAndSam {
+      lee: user(id: '1') {
+        name
+      }
+      sam: user(id: '2') {
+        name
+      }
+    }
   }
-  sam: user(id: "2") {
-    name
-  }
-}
 */
 
-let FetchLeeAndSam = new Query("FetchLeeAndSam");
+const FetchLeeAndSam = new Query('FetchLeeAndSam');
 
-let lee = new Query("user",{id : '1'});
-  lee.setAlias('lee');
-  lee.find({name:true});
-  logger.log("lee",lee.toString());
-  
-let sam = new Query("user","sam");
-  sam.filter({id : '2'});
-  sam.find("name");
-  logger.log("sam",sam+"");
-  
- console.log(FetchLeeAndSam.find(lee,sam)+"");
+const lee = new Nested('user', {
+  id: '1'
+});
+lee.setAlias('lee');
+lee.select({
+  name: true
+});
+
+console.log('lee', lee.toString());
+
+const sam = new Nested('user', 'sam');
+sam.filter({
+  id: '2'
+});
+sam.select('name');
+console.log('sam', sam + '');
+
+console.log(FetchLeeAndSam.select(lee, sam) + '');
